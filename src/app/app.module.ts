680063e0a103411dab5d5aa4fd2371206f3abbdb
grassi <ricardograssi91@gmail.com>
2017-01-25 13:44:13 -0200

    add components of filter and pagination inside of search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { DropdownModule, AlertModule, ButtonsModule, DatepickerModule, CollapseModule, ModalModule} from "ng2-bootstrap/ng2-bootstrap";
+import { DropdownModule, AlertModule, ButtonsModule, DatepickerModule, CollapseModule, ModalModule, PaginationModule} from "ng2-bootstrap/ng2-bootstrap";
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
  
@@ -17,7 +17,10 @@ import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { UserEditComponent } from './users/user-edit/user-edit.component';
 import { ForgotPasswordComponent } from './users/forgot-password/forgot-password.component';
-import { ResultsComponent } from './results/results.component'; 
+import { ResultsComponent } from './results/results.component';
+import { SearchComponent } from './shared/search/search.component';
+import { FilterComponent } from './shared/filter/filter.component';
+import { PaginationComponent } from './shared/pagination/pagination.component'; 
  
 @NgModule({
   declarations: [
@@ -29,7 +32,10 @@ import { ResultsComponent } from './results/results.component';
     FooterComponent,
     UserEditComponent,
     ForgotPasswordComponent,
-    ResultsComponent
+    ResultsComponent,
+    SearchComponent,
+    FilterComponent,
+    PaginationComponent
   ],
   imports: [
     BrowserModule,
@@ -38,7 +44,9 @@ import { ResultsComponent } from './results/results.component';
     routing,
     AlertModule.forRoot(),
     DropdownModule.forRoot(),
-    CollapseModule
+    CollapseModule,
+    DatepickerModule.forRoot(),
+    PaginationModule.forRoot()
   ],
   providers: [PropertiesService, Angular2TokenService],
   bootstrap: [AppComponent]
