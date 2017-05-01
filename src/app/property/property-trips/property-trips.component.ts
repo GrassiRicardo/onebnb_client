17f7f23a530d02c461ec49d6bc7e004f7b098095
grassi <grassiricardorg@gmail.com>
2017-05-01 10:16:43 -0300

    implements cancel reservation

--- src/app/property/property-trips/property-trips.component.ts
+++ src/app/property/property-trips/property-trips.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PropertiesService } from '../../shared/properties.service';
+import { ReservationService } from '../../shared/reservation.service';
 import {ActivatedRoute, Params} from '@angular/router';
-import { Property } from '../../shared/property'; 
+import { Property } from '../../shared/property';
  
 @Component({
   selector: 'app-property-trips',
@@ -13,12 +14,14 @@ export class PropertyTripsComponent implements OnInit {
   private next: any = [];
   private previous: any = [];
   private wishlist: any = [];
-  private pending: any = []; 
+  private pending: any = [];
  
-  constructor(private PropertiesService: PropertiesService, private route: ActivatedRoute) { }
+ 
+  constructor(private PropertiesService: PropertiesService, private route: ActivatedRoute, private ReservationService: ReservationService) { }
  
   ngOnInit() {
-    this.PropertiesService.myTrips().subscribe(data => {
+    this.PropertiesService.myTrips()
+      .subscribe(data => {
         this.next = data['trips']['next'];
         this.previous = data['trips']['previous'];
         this.wishlist = data['trips']['wishlist'];
@@ -34,6 +37,32 @@ export class PropertyTripsComponent implements OnInit {
         this.wishlist.splice(index, 1);
       }
     );
+  }
+ 
+  cancelActive(property)
+  {
+    if (confirm("Você tem certeza que quer deletar a sua reserva na propriedade " + property.name + "? Ela já está confirmada e paga.")) {
+      this.ReservationService.cancel(property.reservation.id)
+        .subscribe(data => {
+          var index = this.next.indexOf(property.id);
+          this.next.splice(index, 1);
+        }
+      );
+    }
+    return false;
+  }
+ 
+  cancelPending(property)
+  {
+    if (confirm("Você tem certeza que quer deletar a sua reserva na propriedade " + property.name + "?")) {
+      this.ReservationService.cancel(property.reservation.id)
+        .subscribe(data => {
+          var index = this.pending.indexOf(property.id);
+          this.pending.splice(index, 1);
+        }
+      );
+    }
+    return false;
   }
  
 }
